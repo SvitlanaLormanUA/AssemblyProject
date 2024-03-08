e9a917b05faf3c9171b3d8ee6f8b5916b9f0a647
Svitlana Lorman <svlormanua@gmail.com>
2024-03-08 15:24:54 +0200

    # Third task, high level Added merge sort Upgraded average value counting

--- Merge.java
+++ Merge.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Merge {
 
@@ -68,17 +66,82 @@ public class Merge {
             countValues.put(key, countValues.getOrDefault(key, 0) + 1);
         }
 
-        int[] averageValues = new int[map.size()];
         int index = 0;
+        double[] averageValues = new double[map.size()];
+        String[] keys = new String[map.size()];
         for (Map.Entry<String, Integer> entry : sumValues.entrySet()) {
             String key = entry.getKey();
             int sum = entry.getValue();
             int count = countValues.get(key);
-            averageValues[index++] = sum / count;
+            averageValues[index] = (double) sum / count;
+            keys[index++] = key;
         }
 
+        mergeSort(averageValues, keys, 0, averageValues.length - 1);
+
+        for (int i = averageValues.length - 1; i >= 0; i--) {
+            System.out.println(keys[i]);
+        }
         for (int j = 0; j < averageValues.length; j++) {
             System.out.println("Середнє значення " + (j + 1) + ": " + averageValues[j]);
         }
     }
-}
+
+    private static void mergeSort(double[] arr, String[] keys, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, keys, left, mid);
+            mergeSort(arr, keys, mid + 1, right);
+            merge(arr, keys, left, mid, right);
+        }
+    }
+
+    private static void merge(double[] arr, String[] keys, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        String[] LKeys = new String[n1];
+        for (int i = 0; i < n1; ++i) {
+            L[i] = (int) arr[left + i];
+            LKeys[i] = keys[left + i];
+        }
+
+        int[] R = new int[n2];
+        String[] RKeys = new String[n2];
+        for (int j = 0; j < n2; ++j) {
+            R[j] = (int) arr[mid + 1 + j];
+            RKeys[j] = keys[mid + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] >= R[j]) {
+                arr[k] = L[i];
+                keys[k] = LKeys[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                keys[k] = RKeys[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            keys[k] = LKeys[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            keys[k] = RKeys[j];
+            j++;
+            k++;
+        }
+    }
+
+    }
