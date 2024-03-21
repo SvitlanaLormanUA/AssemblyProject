412a875d9029ce6409c586c0dbf4aa42769d24ef
Svitlana Lorman <svlormanua@gmail.com>
2024-03-21 21:31:07 +0200

    it is sorting now correctly(?)

--- project.asm
+++ project.asm
@@ -2,76 +2,133 @@
 .stack 100h
 
 .data
-  buffer db 128 dup(?)     ; Buffer for storing input (128 bytes)
-  keys   dw 10000 dup(?)   ; Array to store keys (10000 words)
-  values dw 10000 dup(?)   ; Array to store values (10000 words, assuming they are words)
-  keyCount dw 0            ; Number of keys read (initialized to 0)
-  valueCount dw 0          ; Number of values read (initialized to 0)
-  keyBuffer db 16 dup(?)   ; Buffer for storing the currently processed key (16 bytes)
-  hexBuffer db 5 dup(?)    ; Buffer for storing the hexadecimal representation of values
+filename    db "test.in", 0
+mes         db "Sorted $"
+mesBad      db "File error $"
+handle      dw 0
+
+buffInd     db 0                 ; Index to keep track of the current position in buffer
+oneChar     db 0
+
+keys        db 5000*16 dup(0)
+keyInd      dw 0
+isWord      db 1
+values      db 5000*16 dup(0)
+valInd      dw 0
 
 .code
 main proc
-    mov  ax, @data             ; Load data segment address
-    mov  ds, ax                ; Initialize data segment
+    mov ax, @data
+    mov ds, ax
 
-    mov  ah, 3Fh               ; DOS function for reading from a file
-    mov  bx, 0                 ; File handle (0 = standard input)
-    lea  dx, buffer            ; Pointer to buffer
-    mov  cx, 128               ; Number of bytes to read
-    int  21h                   ; DOS interrupt
+    mov dx, offset filename     ; Address filename with ds:dx 
+    mov ah, 03Dh                ; DOS Open-File function number 
+    mov al, 0                   ; 0 = Read-only access 
+    int 21h                     ; Call DOS to open file 
 
-    cmp  ax, cx                ; Compare bytes read with buffer size
-    jae  checking_eof          ; If end of file reached, proceed to checking EOF
-    mov  cx, ax                ; Update the count of characters read
+    jc error                    ; Call routine to handle errors
 
-    jmp  process_lines         ; Jump to process lines
+    mov [handle], ax            ; Save file handle for later
 
-  checking_eof:     
-    ; Check end of file
-    mov  ah, 3Fh               ; DOS function for checking EOF
-    mov  bx, 0                 ; File handle (0 = standard input)
-    lea  dx, buffer            ; Pointer to buffer
-    mov  cx, 1                 ; Try to read one byte
-    int  21h                   ; DOS interrupt
+; Read file and put characters into buffer
+read_next:
+    mov ah, 3Fh                 ; DOS Read File function number
+    mov bx, [handle]            ; File handle
+    mov cx, 1                   ; 1 byte to read
+    mov dx, offset oneChar      ; Read to ds:dx 
+    int 21h                     ; AX = number of bytes read
 
-    ; If no bytes were read, it means the file has ended
-    cmp  ax, 0
-    jne  process_lines         ; If not EOF, proceed to process lines
-                  
-    ; Otherwise, exit the program
-    mov  ax, 4C00h  
-    int  21h
+    push ax                     ; Preserve ax
+    call procChar               ; Process the read character
+    pop ax                      ; Restore ax
 
-process_lines:
-    mov  si, offset buffer     ; Set SI to the beginning of the buffer
-    mov  di, offset keys       ; Set DI to the beginning of the keys array
-    mov  bx, offset values     ; Set BX to the beginning of the values array
-    mov  cx, 128               ; Initialize CX with the length of the buffer
+    or ax, ax                   ; Check if ax is zero (end of file)
+    jnz read_next               ; If not zero, continue reading
 
-read_and_store:
-    mov  al, [si]              ; Load the character
-    mov  [di], al              ; Store the character in the keys array
-    inc  di                    ; Move to the next position in the keys array
+    ; Clean values last number
+    mov si, offset values
+    mov bx, valInd
+    dec bx
+    add si, bx
+    mov al, 0
+    mov [si], al
 
-    inc  si                    ; Move to the next character
-    mov  al, [si]              ; Load the character
-    mov  [bx], al              ; Store the character in the values array
-    inc  bx                    ; Move to the next position in the values array
+    ; Fill keys array
+    mov ah, 09h                 ; DOS Display String function number
+    mov dx, offset mes
+    int 21h
 
-    inc  si                    ; Move to the next character
-    loop read_and_store        ; Repeat until CX != 0
+    ; Convert values to hexadecimal
+    jmp convert_values_to_hex
 
-    jmp  convert_values_to_hex ; 
+error:
+    mov ah, 09h                 ; DOS Display String function number
+    mov dx, offset mesBad
+    int 21h                     ; Display error message
+
+exit_program:
+    mov ah, 4Ch               ; DOS Terminate Program function number
+    int 21h                     ; Terminate program
+
+procChar proc
+    cmp oneChar, 0Dh            ; Check if carriage return
+    jnz notCR                   ; If not CR, jump to notCR
+    mov isWord, 1               ; Change isWord to 1
+    jmp endProc
+
+notCR:
+    cmp oneChar, 0Ah            ; Check if line feed
+    jnz notLF                   ; If not LF, jump to notLF
+    mov isWord, 1               ; Change isWord to 1
+    jmp endProc
+
+notLF:
+    cmp oneChar, 20h            ; Check if space
+    jnz notSpace                ; If not space, jump to notSpace
+    mov isWord, 0               ; Change isWord to 0
+    jmp endProc
+
+notSpace:
+    cmp isWord, 0               ; Check if isWord is 0
+    jnz itsWord                 ; If not 0, jump to itsWord
+    ; Save char to values
+    mov si, offset values
+    mov bx, valInd
+    add si, bx
+    mov al, oneChar
+    mov [si], al
+    inc valInd
+    jmp endProc
+
+itsWord:
+    ; Save char to keys
+    mov si, offset keys
+    mov bx, keyInd 
+    add si, bx
+    mov al, oneChar
+    mov [si], al
+    inc keyInd 
+
+endProc:
+    ret
+procChar endp   
+
 convert_values_to_hex:
-    mov  al, [si]              ; Load the value
-    call ascii_hex             ; Convert the value to hexadecimal
-    mov  [di], al               ; Store the hexadecimal character in the hexBuffer
-    inc  di                    ; Move to the next position in the hexBuffer
-    inc  si                    ; Move to the next position in the values array
-    loop convert_values_to_hex ; Repeat until CX != 0
+    ; Convert values to hexadecimal
+    mov  cx, valInd             ; Initialize CX with the count of values
+    mov  si, offset values      ; Set SI to the beginning of the values array
+    mov  di, offset values      ; Set DI to the beginning of the values array
 
-    jmp   calculate_average       ; Terminate program
+convert_values_to_hex_loop:
+    mov  al, [si]               ; Load the value
+    call ascii_hex              ; Convert the value to hexadecimal
+    mov  [di], al               ; Store the hexadecimal character in the values buffer
+    inc  di                     ; Move to the next position in the values array
+    inc  si                     ; Move to the next position in the values array
+    loop convert_values_to_hex_loop ; Repeat until CX != 0
+
+    ; Calculate average
+    jmp calculate_average
 
 ascii_hex:        
     push cx
@@ -84,7 +141,6 @@ down:
     mov  cl, 04h
     rol  al, cl
     mov  ch, al
-                    
     mov  ah, 01h
     int  21h
     sub  al, 30h
@@ -95,96 +151,86 @@ d2:
     add  al, ch
     pop  cx
     ret
+
 calculate_average:
     ; Initialize variables
-    mov  cx, keyCount               ; Initialize CX with the count of keys
-    mov  si, offset keys            ; Set SI to the beginning of the keys array
-    mov  di, offset values          ; Set DI to the beginning of the values array
+    mov  cx, keyInd             ; Initialize CX with the count of keys
+    mov  si, offset keys        ; Set SI to the beginning of the keys array
 
-    ; Loop through the keys
 calculate_average_loop:
-    mov  ax, [di]                   ; Load value from values array
-    add  [si + 2], ax               ; Add value to corresponding key's sum
-    add  di, 2                      ; Move to next value
-    add  si, 4                      ; Move to next key and average slot
-    loop calculate_average_loop     ; Repeat until all keys are processed
+    mov  ax, [si]               ; Load value from keys array
+    add  [si + 2], ax           ; Add value to corresponding key's sum
+    add  si, 4                  ; Move to next key and average slot
+    loop calculate_average_loop ; Repeat until all keys are processed
 
     ; Now calculate the average for each key
-    mov  cx, keyCount               ; Reload CX with the count of keys
-    mov  si, offset keys            ; Reset SI to the beginning of the keys array
+    mov  cx, keyInd             ; Reload CX with the count of keys
+    mov  si, offset keys        ; Reset SI to the beginning of the keys array
 
 calculate_average_avg:
-    mov  ax, [si + 2]               ; Load sum of values for the key
-    div  cx                          ; Divide sum by count to get average
-    mov  [si + 2], ax                ; Store the average back to the keys array
-    add  si, 4                       ; Move to next key and average slot
-    loop calculate_average_avg      ; Repeat until all keys are processed
+    mov  ax, [si + 2]           ; Load sum of values for the key
+    div  cx                      ; Divide sum by count to get average
+    mov  [si + 2], ax            ; Store the average back to the keys array
+    add  si, 4                   ; Move to next key and average slot
+    loop calculate_average_avg   ; Repeat until all keys are processed
 
-   
-  ;bubble sort
-  
-  mov  si, offset keys      ; Set SI to the beginning of the key buffer
-  mov  cx, keyCount         ; Number of keys to process
-  dec  cx                   ; Set to count - 1 for loop control
+; Bubble sort
+ 
+    mov  si, offset keys        ; Set SI to the beginning of the keys array
+    mov  cx, keyInd             ; Number of keys to process
+    dec  cx                     ; Set to count - 1 for loop control
 
 outerLoop:
-  push cx                   ; Preserve outer loop counter
+    push cx                     ; Preserve outer loop counter
 
-  lea  si, keys             ; Set SI to the beginning of the key buffer
-  lea  di, keys + 4         ; Set DI to the next key for comparison
-  mov  cx, keyCount - 1     ; Number of key pairs to compare
+    lea  si, keys               ; Set SI to the beginning of the keys array
+    lea  di, keys + 4           ; Set DI to the next key for comparison
+    mov  cx, keyInd - 1         ; Number of key pairs to compare
 
 innerLoop:
-  mov  ax, [si]             ; Load current key's average
-  mov  bx, [di]             ; Load next key's average
-  cmp  ax, bx               ; Compare averages
-  jge  nextPair             ; If current average is greater or equal, proceed to the next pair
+    mov  ax, [si + 2]           ; Load current key's average
+    mov  bx, [di + 2]           ; Load next key's average
+    cmp  ax, bx                 ; Compare averages
+    jge  nextPair               ; If current average is greater or equal, proceed to the next pair
 
-  ; Swap keys
-   mov ax, [si]           ; Load current key into AX
-  mov bx, [di]           ; Load next key into BX
-  mov [si], bx           ; Store next key in current key's place
-  mov [di], ax           ; Store current key in next key's place
+    ; Swap keys
+    mov  ax, [si]               ; Load current key into AX
+    mov  bx, [di]               ; Load next key into BX
+    mov  [si], bx               ; Store next key in current key's place
+    mov  [di], ax               ; Store current key in next key's place
 
-  mov ax, [si+2]         ; Load corresponding count of current key into AX
-  mov bx, [di+2]         ; Load corresponding count of next key into BX
-  mov [si+2], bx         ; Store next key's count in current key's place
-  mov [di+2], ax         ; Store current key's count in next key's place
+    mov  ax, [si + 2]           ; Load corresponding count of current key into AX
+    mov  bx, [di + 2]           ; Load corresponding count of next key into BX
+    mov  [si + 2], bx           ; Store next key's count in current key's place
+    mov  [di + 2], ax           ; Store current key's count in next key's place
 
 nextPair:
-  add  si, 4                ; Move to the next pair
-  add  di, 4                ; Move to the next pair
-  loop innerLoop            ; Repeat until all key pairs are compared
+    add  si, 4                  ; Move to the next pair
+    add  di, 4                  ; Move to the next pair
+    loop innerLoop              ; Repeat until all key pairs are compared
 
-  pop  cx                   ; Restore outer loop counter
-  loop outerLoop            ; Repeat until outer loop counter is zero
+    pop  cx                     ; Restore outer loop counter
+    loop outerLoop              ; Repeat until outer loop counter is zero
 
-  ; Store the number of sorted keys
-  ;  output the sorted keys
-                    mov  si, offset keys      ; Set SI to the beginning of sorted keys (in keyBuffer)
-                    mov  cx, 8                      ; Number of keys to output (assuming 8 keys)
+    ; Output the sorted keys
+    mov  si, offset keys        ; Set SI to the beginning of sorted keys
+    mov  cx, keyInd             ; Number of keys to output
 
-  output_loop:      
-                    mov  ax, [keys]            ; Load key for output from keyBuffer
-                    add  dl, '0'                    ; Convert back to ASCII
-                    mov  ah, 02h                    ; DOS function for displaying character
-                    int  21h                        ; Display character
+output_loop:      
+    mov  ax, [si]               ; Load key for output
+    add  dl, '0'                ; Convert back to ASCII
+    mov  ah, 02h                ; DOS function for displaying character
+    int  21h                    ; Display character
 
-  ; Debugging output to verify the content of the sortedKeys buffer (optional)
-                    mov  dl, ','                    ; Delimiter for debugging output
-                    int  21h                        ; Display delimiter
+    ; Debugging output to verify the content of the sorted keys buffer (optional)
+    mov  dl, ','                ; Delimiter for debugging output
+    int  21h                    ; Display delimiter
 
-                    inc  si                         ; Move to next key in keyBuffer
-                    dec  cx                         ; Decrement the count of keys to output
-                    jnz  output_loop                ; Repeat until all keys are printed
+    add  si, 4                  ; Move to next key
+    loop output_loop            ; Repeat until all keys are printed
 
-  exit_program:     
-  ; Terminate program
-                    mov  ax, 4c00h
-                    int  21h
+    jmp  exit_program           ; Terminate program
 
-main endp
-end main
 
 
 main endp
